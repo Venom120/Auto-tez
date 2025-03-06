9ab7056d8f2452f02ed87cf64fcae86f61b1282f
Venom120 <yatharthjain120@gmail.com>
2025-03-06 23:26:06 +0530

    Captured Window using desired window title name

---
+++ ss.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+void getScreen(const int, const int, const int, const int, unsigned char *);
+void getScreen(const int xx, const int yy, const int W, const int H, /*out*/ unsigned char *data)
+{
+    Display *display = XOpenDisplay(NULL);
+    Window root = DefaultRootWindow(display);
+
+    XImage *image = XGetImage(display, root, xx, yy, W, H, AllPlanes, ZPixmap);
+
+    unsigned long red_mask = image->red_mask;
+    unsigned long green_mask = image->green_mask;
+    unsigned long blue_mask = image->blue_mask;
+    int x, y;
+    int ii = 0;
+    for (y = 0; y < H; y++)
+    {
+        for (x = 0; x < W; x++)
+        {
+            unsigned long pixel = XGetPixel(image, x, y);
+            unsigned char red = (pixel & red_mask) >> 16;
+            unsigned char green = (pixel & green_mask) >> 8;
+            unsigned char blue = (pixel & blue_mask);
+            data[ii + 0] = red;
+            data[ii + 1] = green;
+            data[ii + 2] = blue;
+            ii += 3;
+        }
+    }
+
+    XDestroyImage(image);
+    XCloseDisplay(display);
+}
